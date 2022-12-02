f91d71b52d462281211cbe2218ff43b2ef248845
Alex Barker <abarke28@gmail.com>
2022-12-01 20:21:03 -0800

    Cleanup

---
+++ utils/FileUtils.cs
@@ -0,0 +1,10 @@
+﻿namespace aoc.utils
+{
+    public static class FileUtils
+    {
+        public static List<string> ReadAllLines(string path)
+        {
+            return File.ReadAllLines(path).ToList();
+        }
+    }
+}
