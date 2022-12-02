7153561b84ff91003937ee0a36b21aac63364fff
Alex Barker <abarke28@gmail.com>
2022-12-01 21:38:30 -0800

    Add day 2

--- Program.cs
+++ Program.cs
@@ -1,5 +1,8 @@
 ﻿using aoc.Day_01;
+using aoc.day_02;
 
-Console.WriteLine(Day_01.FindMaxCalories());
-Console.WriteLine(Day_01.FindMaxNCalories(3));
+//Console.WriteLine(Day_01.FindMaxCalories());
+//Console.WriteLine(Day_01.FindMaxNCalories(3));
 
+Console.WriteLine(Day_02.GetScore());
+Console.WriteLine(Day_02.GetScoreV2());
--- day_02/Day_02.cs
+++ day_02/Day_02.cs
@@ -1,13 +1,228 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace aoc.day_02
+﻿namespace aoc.day_02
 {
-    public class Day_02
+    public static class Day_02
     {
+        private enum Rps
+        {
+            Rock = 1,
+            Paper = 2,
+            Scissors = 3
+        }
 
+        private enum Outcome
+        {
+            Lose = 0,
+            Draw = 3,
+            Win = 6
+        }
+
+        private class RpsRound
+        {
+            public Rps Opponent { get; set; }
+            public Rps You { get; set; }
+
+            public int GetScore()
+            {
+                return (int)You + GetRoundScore();
+            }
+
+            public int GetRoundScore()
+            {
+                if (Opponent == You)
+                {
+                    return 3;
+                }
+
+                if (Opponent == Rps.Rock && You == Rps.Paper ||
+                    Opponent == Rps.Paper && You == Rps.Scissors ||
+                    Opponent == Rps.Scissors && You == Rps.Rock)
+                {
+                    return 6;
+                }
+
+                return 0;
+            }
+        }
+
+        public static int GetScore()
+        {
+            return GetRounds().Select(r => r.GetScore()).Sum();
+        }
+
+        public static int GetScoreV2()
+        {
+            return GetRoundsV2().Select(r => r.GetScore()).Sum();
+        }
+
+        private static RpsRound GetRound(string roundLine)
+        {
+            if (roundLine.Length != 3)
+            {
+                throw new ArgumentException(nameof(roundLine));
+            }
+
+            var opponentRps = GetOpponentSelection(roundLine);
+            
+            var you = roundLine[2];
+            Rps youRps;
+
+            switch (you)
+            {
+                case 'X':
+                    youRps = Rps.Rock;
+                    break;
+                case 'Y':
+                    youRps = Rps.Paper;
+                    break;
+                case 'Z':
+                    youRps = Rps.Scissors;
+                    break;
+                default:
+                    throw new Exception($"Unknown you RPS selection: {you}");
+            }
+
+            var round = new RpsRound
+            {
+                Opponent = opponentRps,
+                You = youRps
+            };
+
+            return round;
+        }
+
+        private static RpsRound GetRoundV2(string roundLine)
+        {
+            if(roundLine.Length != 3)
+            {
+                throw new ArgumentException(nameof(roundLine));
+            }
+
+            var opponentRps = GetOpponentSelection(roundLine);
+
+            var desiredOutcome = roundLine[2];
+
+            Outcome desiredOutcomeEnum;
+
+            switch (desiredOutcome)
+            {
+                case 'X':
+                    desiredOutcomeEnum = Outcome.Lose;
+                    break;
+                case 'Y':
+                    desiredOutcomeEnum = Outcome.Draw;
+                    break;
+                case 'Z':
+                    desiredOutcomeEnum = Outcome.Win;
+                    break;
+                default:
+                    throw new Exception($"Unknown outcome selection: {desiredOutcome}");
+            }
+
+            Rps youRps = DetermineWhatToPlay(opponentRps, desiredOutcomeEnum);
+
+            var round = new RpsRound
+            {
+                Opponent = opponentRps,
+                You = youRps
+            };
+
+            return round;
+            
+        }
+
+        private static List<RpsRound> GetRounds()
+        {
+            var roundLines = GetLines();
+
+            var rounds = roundLines.Select(rl => GetRound(rl)).ToList();
+
+            return rounds;
+        }
+
+        private static List<RpsRound> GetRoundsV2()
+        {
+            var roundLines = GetLines();
+
+            var rounds = roundLines.Select(rl => GetRoundV2(rl)).ToList();
+
+            return rounds;
+        }
+
+        private static List<string> GetLines()
+        {
+            var roundLines = File.ReadAllLines("day_02/input.txt");
+
+            return roundLines.ToList();
+        }
+
+        private static Rps GetOpponentSelection(string roundLine)
+        {
+            var opponent = roundLine[0];
+
+            Rps opponentRps;
+
+            switch (opponent)
+            {
+                case 'A':
+                    opponentRps = Rps.Rock;
+                    break;
+                case 'B':
+                    opponentRps = Rps.Paper;
+                    break;
+                case 'C':
+                    opponentRps = Rps.Scissors;
+                    break;
+                default:
+                    throw new Exception($"Unknown opponent RPS selection: {opponent}");
+            }
+
+            return opponentRps;
+        }
+
+        private static Rps DetermineWhatToPlay(Rps opponent, Outcome desiredOutcome)
+        {
+            if (desiredOutcome == Outcome.Draw)
+            {
+                return opponent;
+            }
+
+            if (desiredOutcome == Outcome.Win)
+            {
+                if (opponent == Rps.Rock)
+                {
+                    return Rps.Paper;
+                }
+
+                if (opponent == Rps.Paper)
+                {
+                    return Rps.Scissors;
+                }
+
+                if (opponent == Rps.Scissors)
+                {
+                    return Rps.Rock;
+                }
+            }
+
+            if (desiredOutcome == Outcome.Lose)
+            {
+                if (opponent == Rps.Rock)
+                {
+                    return Rps.Scissors;
+                }
+
+                if (opponent == Rps.Paper)
+                {
+                    return Rps.Rock;
+                }
+
+                if (opponent == Rps.Scissors)
+                {
+                    return Rps.Paper;
+                }
+            }
+
+            throw new Exception($"Unexepcted outcome: {desiredOutcome}");
+        }
     }
 }
