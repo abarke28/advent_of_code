ebcdfc022df3f9782f9ab5cc551c694b727f918a
Alex Barker <abarke28@gmail.com>
2022-12-04 21:35:04 -0800

    Add day 6 prep

---
+++ day_06/Day_06.cs
@@ -0,0 +1,15 @@
+﻿using aoc.utils;
+
+namespace aoc.day_06
+{
+    // https://adventofcode.com/2022/day/6
+    public class Day_06
+    {
+        public static void GetResult()
+        {
+            var lines = FileUtils.ReadAllLines("day_06/input.txt");
+
+
+        }
+    }
+}
