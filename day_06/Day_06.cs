af14efa29b39dfb88d224ddb5e22a83e3f8ad115
Alex Barker <abarke28@gmail.com>
2022-12-05 21:11:12 -0800

    Add day 6

--- day_06/Day_06.cs
+++ day_06/Day_06.cs
@@ -9,6 +9,46 @@ namespace aoc.day_06
         {
             var lines = FileUtils.ReadAllLines("day_06/input.txt");
 
+            var input = lines[0];
+
+            var num1 = FindIndexOfFirstFourUniqueChars(input, 4);
+            var num2 = FindIndexOfFirstFourUniqueChars(input, 14);
+
+            Console.WriteLine(num1);
+            Console.WriteLine(num2);
+        }
+
+        private static int FindIndexOfFirstFourUniqueChars(string s, int numOfChars)
+        {
+            var lastN = new Queue<char>(numOfChars);
+
+            for(int i=0;  i<= s.Length; i++)
+            {
+                if (lastN.Count < numOfChars)
+                {
+                    lastN.Enqueue(s[i]);
+                    continue;
+                }
+                else
+                {
+                    if (IsQueueUnique(lastN))
+                    {
+                        return i;
+                    }
+                    else
+                    {
+                        lastN.Dequeue();
+                        lastN.Enqueue(s[i]);
+                    }
+                }
+            }
+
+            return -1;
+        }
+
+        private static bool IsQueueUnique(Queue<char> queue)
+        {
+            return (queue.Count == queue.ToHashSet().Count);
         }
     }
 }
