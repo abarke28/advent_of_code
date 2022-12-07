e57c249d85dad0358b41b5eb9e0acd0058b900c2
Alex Barker <abarke28@gmail.com>
2022-12-07 13:59:48 -0800

    Set up generator for solvers

---
+++ generate_day.ps1
@@ -0,0 +1,39 @@
+param(
+	[Parameter()]
+	[string]$day
+)
+
+Write-Output "Generating files for day $day...`n"
+
+$folderName = "day_$day"
+$className = "Day_$day.cs"
+$inputName = "input.txt"
+
+New-Item -Path $folderName -ItemType Directory
+
+Push-Location $folderName
+
+New-Item -Name $inputName
+New-Item -Name $className -Value `
+"using aoc.common;
+using aoc.utils;
+
+namespace aoc.day_$day
+{
+    // https://adventofcode.com/2022/day/$day
+    public class Day_$day : ISolver
+    {
+        public void Solve()
+        {
+            var lines = FileUtils.ReadAllLines(`"day_$day/input.txt`");
+
+            Console.WriteLine(`"Hello World`");
+        }
+    }
+}
+"
+
+Write-Output "Generated files for day $day`:"
+Get-ChildItem
+
+Pop-Location
