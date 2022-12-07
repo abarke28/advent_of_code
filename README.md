23b07b95d74374fd85d9622cb24da525a438ef6d
Alex Barker <abarke28@gmail.com>
2022-12-07 09:17:42 -0800

    Cleanup day 5

--- README.md
+++ README.md
@@ -8,10 +8,10 @@ A daily coding challenge for each day in December leading up to Christmas. Each 
 
 | Day | Story | Part 1 | Part 2 | command to run | challenge level | What I learnt/used
 | --- | --- | --- | --- | --- | --- | --- |
-| 1 | [Calorie Counting](https://adventofcode.com/2022/day/1) | ✔️ | ✔️  | `dotnet solve 1` | ⭐ | Iteration strategies.
-| 2 | [Rock Paper Scissors](https://adventofcode.com/2022/day/2) | ✔️ | ✔️ | `dotnet solve 2` | ⭐⭐ | Modulo math.
-| 3 | [Rucksack Reorganization](https://adventofcode.com/2022/day/3) | ✔️ | ✔️ | `dotnet solve 3` | ⭐⭐ | Set theory.
-| 4 | [Camp Cleanup](https://adventofcode.com/2022/day/4) | ✔️ | ✔️ | `dotnet solve 4` | ⭐⭐ | Text parsing.
-| 5 | [Supply Stacks](https://adventofcode.com/2022/day/5) | ✔️ | ✔️ | `dotnet solve 5`| ⭐⭐ | Queue exercise.
-| 6 | [Tuning Trouble](https://adventofcode.com/2022/day/6) | ✔️ | ✔️ | `dotnet solve 6` | ⭐⭐ | Windowing strategies for enumerables.
-| 7 | [File System Building](https://adventofcode.com/2022/day/7) | ✔️ | ✔️ | `dotnet solve 7` | ⭐⭐⭐ | Tree structure and recursion.
+| 1 | [Calorie Counting](https://adventofcode.com/2022/day/1) | ✔️ | ✔️  | `dotnet run 1` | ⭐ | Iteration strategies.
+| 2 | [Rock Paper Scissors](https://adventofcode.com/2022/day/2) | ✔️ | ✔️ | `dotnet run 2` | ⭐⭐ | Modulo math.
+| 3 | [Rucksack Reorganization](https://adventofcode.com/2022/day/3) | ✔️ | ✔️ | `dotnet run 3` | ⭐⭐ | Set theory.
+| 4 | [Camp Cleanup](https://adventofcode.com/2022/day/4) | ✔️ | ✔️ | `dotnet run 4` | ⭐⭐ | Text parsing.
+| 5 | [Supply Stacks](https://adventofcode.com/2022/day/5) | ✔️ | ✔️ | `dotnet run 5`| ⭐⭐ | Queue exercise.
+| 6 | [Tuning Trouble](https://adventofcode.com/2022/day/6) | ✔️ | ✔️ | `dotnet run 6` | ⭐⭐ | Windowing strategies for enumerables.
+| 7 | [File System Building](https://adventofcode.com/2022/day/7) | ✔️ | ✔️ | `dotnet run 7` | ⭐⭐⭐ | Tree structure and recursion.
--- day_05/Day_05.cs
+++ day_05/Day_05.cs
@@ -5,23 +5,13 @@ namespace aoc.day_05
     // https://adventofcode.com/2022/day/5
     public class Day_05
     {
-        private static List<Stack<char>> CrateStacks = new List<Stack<char>>
-        {
-            new Stack<char>( new List<char> { 'F', 'H', 'B', 'V', 'R', 'Q', 'D', 'P' }),
-            new Stack<char>( new List<char> { 'L', 'D', 'Z', 'Q', 'W', 'V' }),
-            new Stack<char>( new List<char> { 'H', 'L', 'Z', 'Q', 'G', 'R', 'P', 'C' }),
-            new Stack<char>( new List<char> { 'R', 'D', 'H', 'F', 'J', 'V', 'B' }),
-            new Stack<char>( new List<char> { 'Z', 'W', 'L', 'C' }),
-            new Stack<char>( new List<char> { 'J', 'R', 'P', 'N', 'T', 'G', 'V', 'M' }),
-            new Stack<char>( new List<char> { 'J', 'R', 'L', 'V', 'M', 'B', 'S' }),
-            new Stack<char>( new List<char> { 'D', 'P', 'J' }),
-            new Stack<char>( new List<char> { 'D', 'C', 'N', 'W', 'V' })
-        };
+        private static List<Stack<char>> CrateStacks = BuildCrateStacksInitialState();
 
         public static void GetResult()
         {
             var lines = FileUtils.ReadAllLines("day_05/input_2.txt");
 
+            // Part 1
             foreach(var line in lines)
             {
                 PerformLineOperation(line);
@@ -31,9 +21,22 @@ namespace aoc.day_05
             {
                 Console.Write(stack.Peek());
             }
+            Console.WriteLine();
+
+            // Part 2
+            CrateStacks = BuildCrateStacksInitialState();
+            foreach (var line in lines)
+            {
+                PerformLineOperation(line, crateMoveStrat: 2);
+            }
+
+            foreach (var stack in CrateStacks)
+            {
+                Console.Write(stack.Peek());
+            }
         }
 
-        private static void PerformLineOperation(string l)
+        private static void PerformLineOperation(string l, int crateMoveStrat = 1)
         {
             var words = l.Split(' ');
 
@@ -45,8 +48,14 @@ namespace aoc.day_05
             var sourceStack = int.Parse(words[sourceIndex]) - 1;
             var destinationStack = int.Parse(words[destinationIndex]) - 1;
 
-            //MoveCratesV1(moveCount, sourceStack, destinationStack);
-            MoveCratesV2(moveCount, sourceStack, destinationStack);
+            if (crateMoveStrat == 1)
+            {
+                MoveCratesV1(moveCount, sourceStack, destinationStack);
+            }
+            else
+            {
+                MoveCratesV2(moveCount, sourceStack, destinationStack);
+            }
         }
 
         private static void MoveCratesV1(int moveCount, int sourceStack, int destinationStack)
@@ -80,6 +89,22 @@ namespace aoc.day_05
             {
                 CrateStacks[destinationStack].Push(crate);
             }
+        }
+
+        private static List<Stack<char>> BuildCrateStacksInitialState()
+        {
+            return new List<Stack<char>>
+            {
+                new Stack<char>( new List<char> { 'F', 'H', 'B', 'V', 'R', 'Q', 'D', 'P' }),
+                new Stack<char>( new List<char> { 'L', 'D', 'Z', 'Q', 'W', 'V' }),
+                new Stack<char>( new List<char> { 'H', 'L', 'Z', 'Q', 'G', 'R', 'P', 'C' }),
+                new Stack<char>( new List<char> { 'R', 'D', 'H', 'F', 'J', 'V', 'B' }),
+                new Stack<char>( new List<char> { 'Z', 'W', 'L', 'C' }),
+                new Stack<char>( new List<char> { 'J', 'R', 'P', 'N', 'T', 'G', 'V', 'M' }),
+                new Stack<char>( new List<char> { 'J', 'R', 'L', 'V', 'M', 'B', 'S' }),
+                new Stack<char>( new List<char> { 'D', 'P', 'J' }),
+                new Stack<char>( new List<char> { 'D', 'C', 'N', 'W', 'V' })
+            };
         }
     }
 }
