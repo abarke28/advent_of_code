7f38d727c0d1f04e542bebc959d22368db49c884
Alex Barker <abarke28@gmail.com>
2022-12-07 22:10:02 -0800

    Add day 9 prep

---
+++ day_09/Day_09.cs
@@ -0,0 +1,16 @@
+using aoc.common;
+using aoc.utils;
+
+namespace aoc.day_09
+{
+    // https://adventofcode.com/2022/day/09
+    public class Day_09 : ISolver
+    {
+        public void Solve()
+        {
+            var lines = FileUtils.ReadAllLines("day_09/input.txt");
+
+            Console.WriteLine("Hello World");
+        }
+    }
+}
