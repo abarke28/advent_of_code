a66d37fff83bad1f03beaac9a4a1b81ebb39b803
Alex Barker <abarke28@gmail.com>
2022-12-09 09:31:25 -0800

    Add Vector2D

---
+++ utils/Vector2D.cs
@@ -0,0 +1,77 @@
+﻿namespace v1oc.utils
+{
+    public readonly struct Vector2D : IEquatable<Vector2D>
+    {
+        public static readonly Vector2D Zero = new(0, 0);
+        public static readonly Vector2D Up = new(0, 1);
+        public static readonly Vector2D Down = new(0, -1);
+        public static readonly Vector2D Left = new(-1, 0);
+        public static readonly Vector2D Right = new(1, 0);
+
+        public int X { get; }
+        public int Y { get; }
+        public string Id => ToString();
+
+        public Vector2D(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public Vector2D(Vector2D other)
+        {
+            X = other.X;
+            Y = other.Y;
+        }
+
+        public bool Equals(Vector2D other)
+        {
+            return X == other.X && Y == other.Y;
+        }
+
+        public override bool Equals(object? obj)
+        {
+            return obj is Vector2D other && Equals(other);
+        }
+
+        public override int GetHashCode()
+        {
+            return 17 * X + 31 * Y;
+        }
+
+        public override string ToString()
+        {
+            return $"[{X},{Y}]";
+        }
+
+        public static Vector2D operator +(Vector2D v1, Vector2D v2)
+        {
+            return new Vector2D(v1.X + v2.X, v1.Y + v2.Y);
+        }
+
+        public static Vector2D operator -(Vector2D v1, Vector2D v2)
+        {
+            return new Vector2D(v1.X - v2.X, v1.Y - v2.Y);
+        }
+
+        public static bool operator ==(Vector2D v1, Vector2D v2)
+        {
+            return v1.Equals(v2);
+        }
+
+        public static bool operator !=(Vector2D v1, Vector2D v2)
+        {
+            return !(v1 == v2);
+        }
+
+        public static Vector2D operator *(int k, Vector2D v)
+        {
+            return new Vector2D(k * v.X, k * v.Y);
+        }
+
+        public static Vector2D operator *(Vector2D v, int k)
+        {
+            return k * v;
+        }
+    }
+}
