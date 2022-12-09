94517584f6428420e4c561f1be9695c47ad96f4c
Alex Barker <abarke28@gmail.com>
2022-12-09 09:58:36 -0800

    Add vector extensions

---
+++ utils/Extensions.cs
@@ -0,0 +1,22 @@
+﻿using aoc.utils;
+
+namespace aoc.utils
+{
+    public static class Extensions
+    {
+        public static bool IsAdjacent(this Vector2D v, Vector2D other)
+        {
+            return Math.Abs(v.X - other.X) <= 1 && Math.Abs(v.Y - other.Y) <= 1;
+        }
+
+        public static bool IsInSameRow(this Vector2D v, Vector2D other)
+        {
+            return v.Y == other.Y;
+        }
+
+        public static bool IsInSameColumn(this Vector2D v, Vector2D other)
+        {
+            return v.X == other.X;
+        }
+    }
+}
