705457f8f56e70d515183520525637366955fd02
Alex Barker <abarke28@gmail.com>
2022-12-12 13:13:50 -0800

    Add day 13 prep

---
+++ 2022/day_13/Day_13.cs
@@ -0,0 +1,16 @@
+using aoc.common;
+using aoc.utils;
+
+namespace aoc.y2022.day_13
+{
+    // https://adventofcode.com/2022/day/13
+    public class Day_13 : ISolver
+    {
+        public void Solve()
+        {
+            var lines = FileUtils.ReadAllLines("2022/day_13/input.txt");
+
+            Console.WriteLine("Hello World");
+        }
+    }
+}
