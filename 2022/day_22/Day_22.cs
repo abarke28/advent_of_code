05c0330cba588b86d327c9a5cdc03699c31194a9
Alex Barker <abarke28@gmail.com>
2022-12-23 17:23:50 -0800

    Finish day 22

--- 2022/day_22/Day_22.cs
+++ 2022/day_22/Day_22.cs
@@ -35,45 +35,61 @@ namespace aoc.y2022.day_22
             { Vector2D.Down, 3 }
         };
 
+        private static readonly Vector2D Up = Vector2D.Down;
+        private static readonly Vector2D Down = Vector2D.Up;
+        private static readonly Vector2D Left = Vector2D.Left;
+        private static readonly Vector2D Right = Vector2D.Right;
+
         public void Solve()
         {
             var lines = FileUtils.ReadAllLines("2022/day_22/input.txt");
 
             var (grid, instructions) = ParseInput(lines);
-            grid.PrintGrid();
 
             var startingPosition = grid.GetRow(0).WithIndex().First(rwi => rwi.item == Valid).index;
             var startingVector = new Vector2D(startingPosition, 0);
             var startingHeading = Vector2D.Right;
 
-            var (finalPosition, finalHeading) = ExecuteInstructions(grid, instructions, startingVector, startingHeading);
+            var (finalPosition, finalHeading) = ExecuteInstructions(grid, instructions, startingVector, startingHeading, wrap3D: false);
 
             var score = CalculateScore(finalPosition, finalHeading);
-            Console.WriteLine(score);
+            Console.WriteLine($"Final position: {finalPosition}, final heading: {finalHeading}, score: {score}");
+
+            var (_, instructions3D) = ParseInput(lines);
+            var (finalPosition3D, finalHeading3D) = ExecuteInstructions(grid, instructions3D, startingVector, startingHeading, wrap3D: true);
+
+            var score3D = CalculateScore(finalPosition3D, finalHeading3D);
+            Console.WriteLine($"Final position: {finalPosition3D}, final heading: {finalHeading3D}, score: {score3D}");
         }
 
-        private static (Vector2D finalPosition, Vector2D finalHeading) ExecuteInstructions(Grid<char> grid, IEnumerable<Instruction> instructions, Vector2D startingPosition, Vector2D startingHeading)
+        private static (Vector2D finalPosition, Vector2D finalHeading) ExecuteInstructions(
+            Grid<char> grid,
+            IEnumerable<Instruction> instructions, 
+            Vector2D startingPosition, 
+            Vector2D startingHeading,
+            bool wrap3D)
         {
             var currentHeading = startingHeading;
             var currentPosition = startingPosition;
 
             foreach (var instruction in instructions)
             {
-                (currentPosition, currentHeading) = ExecuteInstruction(grid, instruction, currentPosition, currentHeading);
-                Console.WriteLine($"Now at {currentPosition}, with heading {currentHeading}");
+                (currentPosition, currentHeading) = ExecuteInstruction(grid, instruction, currentPosition, currentHeading, wrap3D);
             }
 
             return (currentPosition, currentHeading);
         }
 
-        private static (Vector2D newPosition, Vector2D newHeading) ExecuteInstruction(Grid<char> grid, Instruction instruction, Vector2D currentPosition, Vector2D currentHeading)
+        private static (Vector2D newPosition, Vector2D newHeading) ExecuteInstruction(
+            Grid<char> grid,
+            Instruction instruction,
+            Vector2D currentPosition,
+            Vector2D currentHeading,
+            bool wrap3D)
         {
-            Console.WriteLine($"Instruction: Move: {instruction.Count}, Turn: {instruction.Direction}");
-
             if (instruction.Direction != Turn.None)
             {
                 var adjustedHeading = CalculateTurn(currentHeading, instruction.Direction);
-                Console.WriteLine($"Heading adjusted to {adjustedHeading}, still at {currentPosition}");
 
                 return (currentPosition, adjustedHeading);
             }
@@ -90,13 +106,13 @@ namespace aoc.y2022.day_22
                 
                 var isInBounds = grid.IsInBounds(nextTarget);
 
-                Console.WriteLine($"At {currentPosition} with heading {currentHeading}. Next target {nextTarget}");
-
                 if (isInBounds && grid.GetValue(nextTarget) == Valid)
                 {
                     currentPosition = nextTarget;
                     currentHeading = nextHeading;
                     newPosition = currentPosition;
+                    newHeading = nextHeading;
+
                     instruction.Count--;
                     continue;
                 }
@@ -106,7 +122,8 @@ namespace aoc.y2022.day_22
                 }
                 else if (!isInBounds || grid.GetValue(nextTarget) == InValid)
                 {
-                    var (canMove, wrappedTarget, wrappedHeading) = ComputeWrap(grid, currentPosition, currentHeading);
+                    var (canMove, wrappedTarget, wrappedHeading) = 
+                        wrap3D ? ComputeWrap3D(grid, currentPosition, currentHeading) : ComputeWrap2D(grid, currentPosition, currentHeading);
 
                     if (!canMove)
                     {
@@ -127,7 +144,129 @@ namespace aoc.y2022.day_22
             return (newPosition, newHeading);
         }
 
-        private static (bool canMove, Vector2D wrappedPosition, Vector2D wrappedHeading) ComputeWrap(Grid<char> grid, Vector2D currentPosition, Vector2D currentHeading)
+        private static (bool canMove, Vector2D wrappedPosition, Vector2D wrappedHeading) ComputeWrap3D(Grid<char> grid, Vector2D currentPosition, Vector2D currentHeading)
+        {
+            /*
+                 ..0 .50 100 150
+             ..0 ... .1. .2. ...
+             .50 ... .3. ... ...
+             100 .4. .5. ... ...
+             150 .6. ... ... ...
+             200 ... ... ... ...
+                                  */
+
+            var (x, y) = currentPosition;
+
+            var wrappedPosition = Vector2D.Zero;
+            var wrappedHeading = Vector2D.Zero;
+
+            // 1 > 6
+            if (x is >= 50 and <= 99 && y is 0 && currentHeading == Up)
+            {
+                wrappedPosition = new Vector2D(0, x + 100);
+                wrappedHeading = Right;
+            }
+
+            // 6 > 1
+            if (x is 0 && y is >= 150 and <= 199 && currentHeading == Left)
+            {
+                wrappedPosition = new Vector2D(y - 100, 0);
+                wrappedHeading = Down;
+            }
+
+            // 1 > 4
+            if (x is 50 && y is >= 0 and <= 49 && currentHeading == Left)
+            {
+                wrappedPosition = new Vector2D(0, 149 - y);
+                wrappedHeading = Right;
+            }
+
+            // 4 > 1
+            if (x is 0 && y is >= 100 and <= 149 && currentHeading == Left)
+            {
+                wrappedPosition = new Vector2D(50, 149 - y);
+                wrappedHeading = Right;
+            }
+
+            // 3 > 4
+            if (x is 50 && y is >= 50 and <= 99 && currentHeading == Left)
+            {
+                wrappedPosition = new Vector2D(y - 50, 100);
+                wrappedHeading = Down;
+            }
+
+            // 4 > 3
+            if (x is >= 0 and <= 49 && y is 100 && currentHeading == Up)
+            {
+                wrappedPosition = new Vector2D(50, x + 50);
+                wrappedHeading = Right;
+            }
+
+            // 2 > 3
+            if (x is >= 100 and <= 149 && y == 49 && currentHeading == Down)
+            {
+                wrappedPosition = new Vector2D(99, x - 50);
+                wrappedHeading = Left;
+            }
+
+            // 3 > 2
+            if (x is 99 && y is >= 50 and <= 99 && currentHeading == Right)
+            {
+                wrappedPosition = new Vector2D(y + 50, 49);
+                wrappedHeading = Up;
+            }
+
+            // 5 > 6
+            if (x is >= 50 and <= 99 && y is 149 && currentHeading == Down)
+            {
+                wrappedPosition = new Vector2D(49, x + 100);
+                wrappedHeading = Left;
+            }
+
+            // 6 > 5
+            if (x is 49 && y is >= 150 and <= 199 && currentHeading == Right)
+            {
+                wrappedPosition = new Vector2D(y - 100, 149);
+                wrappedHeading = Up;
+            }
+
+            // 2 > 6
+            if (x is >= 100 and <= 149 && y is 0 && currentHeading == Up)
+            {
+                wrappedPosition = new Vector2D(x - 100, 199);
+                wrappedHeading = Up;
+            }
+
+            // 6 > 2
+            if (x is >= 0 and <= 49 && y is 199 && currentHeading == Down)
+            {
+                wrappedPosition = new Vector2D(x + 100, 0);
+                wrappedHeading = Down;
+            }
+
+            // 2 > 5
+            if (x is 149 && y is >= 0 and <= 49 && currentHeading == Right)
+            {
+                wrappedPosition = new Vector2D(99, 149 - y);
+                wrappedHeading = Left;
+            }
+
+            // 5 > 2
+            if (x is 99 && y is >= 100 and <= 149 && currentHeading == Right)
+            {
+                wrappedPosition = new Vector2D(149, 149 - y);
+                wrappedHeading = Left;
+            }
+
+            if (!grid.IsInBounds(wrappedPosition) || grid.GetValue(wrappedPosition) == Wall)
+                {
+                    return (false, wrappedPosition, wrappedHeading);
+                }
+
+            return (true, wrappedPosition, wrappedHeading);
+        }
+
+        private static (bool canMove, Vector2D wrappedPosition, Vector2D wrappedHeading) ComputeWrap2D(Grid<char> grid, Vector2D currentPosition, Vector2D currentHeading)
         {
             // Heading left or right
             if (currentHeading.Y == 0)
@@ -192,15 +331,6 @@ namespace aoc.y2022.day_22
             var score = _headingScoreMap[heading] + ((position.X + 1) * ColScore) + ((position.Y + 1) * RowScore);
 
             return score;
-        }
-
-        private static IDictionary<(Vector2D position, Vector2D heading), (Vector2D position, Vector2D heading)> Compute3DWraps(Grid<char> grid)
-        {
-            var wraps = new Dictionary<(Vector2D position, Vector2D heading), (Vector2D position, Vector2D heading)>();
-
-            // TODO - Compute wraps.
-
-            return wraps;
         }
 
         private static (Grid<char> grid, IList<Instruction> instructions) ParseInput(IList<string> lines)
--- utils/Vector2D.cs
+++ utils/Vector2D.cs
@@ -70,6 +70,12 @@
             }
         }
 
+        public void Deconstruct(out int x, out int y)
+        {
+            x = X;
+            y = Y;
+        }
+
         public static Vector2D operator +(Vector2D v1, Vector2D v2)
         {
             return new Vector2D(v1.X + v2.X, v1.Y + v2.Y);
