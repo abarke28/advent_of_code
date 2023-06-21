37e81e0c7f4bc7f4d3ea1fe1718d52fc32eff2dd
Alex Barker <abarke28@gmail.com>
2023-06-20 19:47:58 -0700

    2021 day 16 wip

---
+++ 2021/day_16/Day_16.cs
@@ -0,0 +1,25 @@
+using aoc.common;
+using aoc.utils;
+
+namespace aoc.y2021.day_16
+{
+    // https://adventofcode.com/2021/day/16
+    public class Day_16 : ISolver
+    {
+        public void Solve()
+        {
+            var packet = FileUtils.ReadAllLines("2021/day_16/input.txt").Single();
+            var bits = ConvertHexToBits(packet);
+
+        }
+
+        public static string ConvertHexToBits(string hex)
+        {
+            long longVal = Convert.ToInt64(hex, fromBase: 16);
+
+            var bitString = Convert.ToString(longVal, toBase: 2);
+
+            return bitString;
+        }
+    }
+}
