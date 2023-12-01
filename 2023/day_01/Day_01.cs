f6be6ba26ad8cef0283fb7eaa4da62f7fbca9443
Alex Barker <abarke28@gmail.com>
2023-11-30 20:00:10 -0800

    Fix year

---
+++ 2023/day_01/Day_01.cs
@@ -0,0 +1,16 @@
+using aoc.common;
+using aoc.utils;
+
+namespace aoc.y2023.day_01
+{
+    // https://adventofcode.com/2023/day/01
+    public class Day_01 : ISolver
+    {
+        public void Solve()
+        {
+            var lines = FileUtils.ReadAllLines("2023/day_01/input.txt");
+
+            Console.WriteLine("Hello World");
+        }
+    }
+}
