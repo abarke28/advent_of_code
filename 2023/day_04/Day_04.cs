79f2651c35fcae99911263fffb7cdda57fb45441
Alex Barker <abarke28@gmail.com>
2023-12-03 21:35:51 -0800

    Add day 4 solution, day 5 prep

--- 2023/day_04/Day_04.cs
+++ 2023/day_04/Day_04.cs
@@ -1,18 +1,77 @@
 using aoc.common;
+using aoc.utils.extensions;
 
 namespace aoc.y2023.day_04
 {
     // https://adventofcode.com/2023/day/04
     public class Day_04 : ISolver
     {
+        private class Card
+        {
+            public int Id { get; set; }
+            public List<int> WinningNums { get; set; } = new List<int>();
+            public List<int> ActualNums { get; set; } = new List<int>();
+
+            public int GetMatchCount()
+            {
+                return WinningNums.Intersect(ActualNums).Count();
+            }
+
+            public int GetScore()
+            {
+                var count = GetMatchCount();
+
+                return count == 0 ? 0 : (int)Math.Pow(2, count - 1);
+            }
+        }
+
         public object Part1(IList<string> lines)
         {
-            return 0;
+            var tickets = ParseTickets(lines);
+
+            var scores = tickets.Select(t => t.GetScore());
+
+            return scores.Sum();
         }
 
         public object Part2(IList<string> lines)
         {
-            return 0;
+            var cards = ParseTickets(lines);
+
+            var cardCount = cards.Count;
+            var cardsToProcess = new Queue<Card>(cards);
+
+            while (cardsToProcess.Count > 0)
+            {
+                var current = cardsToProcess.Dequeue();
+
+                var newCardsCount = current.GetMatchCount();
+
+                if (newCardsCount > 0)
+                {
+                    cardCount += newCardsCount;
+
+                    var newCards = cards.Skip(current.Id).Take(newCardsCount);
+
+                    foreach (var newCard in newCards)
+                    {
+                        cardsToProcess.Enqueue(newCard);
+                    }
+                }
+            }
+
+            return cardCount;
+        }
+
+        private static List<Card> ParseTickets(IList<string> lines)
+        {
+            return lines
+                .Select(l => l.Split(':').Last())
+                .Select(l => l.Split("|"))
+                .Select(l => (l[0].ReadAllNumbers().ToList(), l[1].ReadAllNumbers().ToList()))
+                .WithIndex()
+                .Select(c => new Card { Id = c.index + 1, WinningNums = c.item.Item1, ActualNums = c.item.Item2 })
+                .ToList();
         }
     }
 }
---
+++ 2023/day_05/Day_05.cs
@@ -0,0 +1,18 @@
+using aoc.common;
+
+namespace aoc.y2023.day_05
+{
+    // https://adventofcode.com/2023/day/05
+    public class Day_05 : ISolver
+    {
+        public object Part1(IList<string> lines)
+        {
+            return 0;
+        }
+
+        public object Part2(IList<string> lines)
+        {
+            return 0;
+        }
+    }
+}
