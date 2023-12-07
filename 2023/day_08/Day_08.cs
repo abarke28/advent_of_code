40f4fafa99b6f0a7d42f777e2d8bff6a627a525e
Alex Barker <abarke28@gmail.com>
2023-12-06 22:42:46 -0800

    Add day 7, day 8 prep

---
+++ 2023/day_08/Day_08.cs
@@ -0,0 +1,18 @@
+using aoc.common;
+
+namespace aoc.y2023.day_08
+{
+    // https://adventofcode.com/2023/day/08
+    public class Day_08 : ISolver
+    {
+        public object Part1(IList<string> lines)
+        {
+            return 0;
+        }
+
+        public object Part2(IList<string> lines)
+        {
+            return 0;
+        }
+    }
+}
