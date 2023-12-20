f7f35f6ead723a7ffbc2233f03b420f6323ece4e
Alex Barker <abarke28@gmail.com>
2023-12-19 20:37:01 -0800

    Add day 20 base

---
+++ 2023/Day_20/Day_20.cs
@@ -0,0 +1,18 @@
+using Aoc.Common;
+
+namespace Aoc.Y2023.Day_20
+{
+    // https://adventofcode.com/2023/day/20
+    public class Day_20 : ISolver
+    {
+        public object Part1(IList<string> lines)
+        {
+            return 0;
+        }
+
+        public object Part2(IList<string> lines)
+        {
+            return 0;
+        }
+    }
+}
