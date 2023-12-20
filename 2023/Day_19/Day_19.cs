ce9ca1d0fb4b9f789f314acb0fc0605846f300e4
Alex Barker <abarke28@gmail.com>
2023-12-19 20:36:52 -0800

    Day 19 p2 wip

--- 2023/Day_19/Day_19.cs
+++ 2023/Day_19/Day_19.cs
@@ -7,6 +7,60 @@ namespace Aoc.Y2023.Day_19
     // https://adventofcode.com/2023/day/19
     public class Day_19 : ISolver
     {
+        private class AttributeRange
+        {
+            public int LowerBound { get; set; }
+            public int UpperBound { get; set; }
+
+            public int Length => UpperBound - LowerBound;
+
+            public AttributeRange(int lowerBound, int upperBound)
+            {
+                LowerBound = lowerBound;
+                UpperBound = upperBound;
+            }
+
+            public AttributeRange GetPassingRange(int num, bool greaterThan)
+            {
+                if (greaterThan)
+                {
+                    if (num >= UpperBound) return new AttributeRange(0, 0);
+
+                    if (num < LowerBound) return new AttributeRange(LowerBound, UpperBound);
+
+                    return new AttributeRange(num + 1, UpperBound);
+                }
+                else
+                {
+                    if (num >= UpperBound) return new AttributeRange(LowerBound, UpperBound);
+
+                    if (num <= LowerBound) return new AttributeRange(0, 0);
+
+                    return new AttributeRange(LowerBound, num - 1);
+                }
+            }
+
+            public AttributeRange GetFailingRange(int num, bool greaterThan)
+            {
+                if (greaterThan)
+                {
+                    if (num >= UpperBound) return new AttributeRange(LowerBound, UpperBound);
+
+                    if (num <= LowerBound) return new AttributeRange(0, 0);
+
+                    return new AttributeRange(LowerBound, num);
+                }
+                else
+                {
+                    if (num >= UpperBound) return new AttributeRange(0, 0);
+
+                    if (num < LowerBound) return new AttributeRange(LowerBound, UpperBound);
+
+                    return new AttributeRange(num, UpperBound);
+                }
+            }
+        }
+
         private class Part
         {
             public Dictionary<string, int> Attributes { get; set; } = new Dictionary<string, int>();
@@ -131,9 +185,90 @@ namespace Aoc.Y2023.Day_19
 
         public object Part2(IList<string> lines)
         {
+            var (workflows, parts) = ParseLines(lines);
+            var workflowsDictionary = workflows.ToDictionary(w => w.Name, w => w);
 
+            var startingWorkflow = workflowsDictionary["in"];
 
-            return 0;
+            var startingRanges = new string[] { "x", "m", "a", "s" }.ToDictionary(c => c, _ => new AttributeRange(1, 4001));
+
+            var acceptanceRanges = GetAcceptanceRanges(startingWorkflow, workflowsDictionary, ruleIndex: 0, startingRanges);
+
+            var possibilites = acceptanceRanges.Select(ar => 1L * ar["x"].Length * ar["m"].Length * ar["a"].Length * ar["s"].Length);
+
+            var sum = possibilites.Sum();
+
+            return sum;
+        }
+
+        private static List<Dictionary<string, AttributeRange>> GetAcceptanceRanges(
+            Workflow workflow, Dictionary<string, Workflow> workflows, int ruleIndex, Dictionary<string, AttributeRange> currentRanges)
+        {
+            var rule = workflow.Rules[ruleIndex];
+
+            var ranges = new List<Dictionary<string, AttributeRange>>();
+
+            if (rule.Unconditional && rule.SendTo == "R")
+            {
+                // No-op
+                //ranges.Add(new string[] { "x", "m", "a", "s" }.ToDictionary(c => c, _ => new AttributeRange(0, 0)));
+            }
+            else if (rule.Unconditional && rule.SendTo == "A")
+            {
+                ranges.Add(currentRanges);
+            }
+            else if (rule.Unconditional)
+            {
+                var nextDestination = rule.SendTo;
+                var nextWorkflow = workflows[nextDestination];
+
+                ranges.AddRange(GetAcceptanceRanges(nextWorkflow, workflows, 0, currentRanges));
+            }
+            else
+            {
+                var partAttribute = rule.PartAttribute!;
+
+                var passingRange = currentRanges[partAttribute].GetPassingRange(rule.Threshold, rule.GreaterThan);
+                var failingRange = currentRanges[partAttribute].GetFailingRange(rule.Threshold, rule.GreaterThan);
+
+                var nextDestination = rule.SendTo;
+
+                if (nextDestination == "A")
+                {
+                    currentRanges[partAttribute] = passingRange;
+                    ranges.Add(currentRanges);
+
+                    var failingCopy = currentRanges.ToDictionary(kvp => kvp.Key, kvp => kvp.Value);
+                    failingCopy[partAttribute] = failingRange;
+
+                    ranges.AddRange(GetAcceptanceRanges(workflow, workflows, ruleIndex + 1, failingCopy));
+                }
+                else if (nextDestination == "R")
+                {
+                    // no-op for passing.
+
+                    var failingCopy = currentRanges.ToDictionary(kvp => kvp.Key, kvp => kvp.Value);
+                    failingCopy[partAttribute] = failingRange;
+
+                    ranges.AddRange(GetAcceptanceRanges(workflow, workflows, ruleIndex + 1, failingCopy));
+                }
+                else
+                {
+                    var passingCopy = currentRanges.ToDictionary(kvp => kvp.Key, kvp => kvp.Value);
+                    passingCopy[partAttribute] = passingRange;
+
+                    var nextWorkflow = workflows[nextDestination];
+
+                    ranges.AddRange(GetAcceptanceRanges(nextWorkflow, workflows, 0, passingCopy));
+
+                    var failingCopy = currentRanges.ToDictionary(kvp => kvp.Key, kvp => kvp.Value);
+                    failingCopy[partAttribute] = failingRange;
+
+                    ranges.AddRange(GetAcceptanceRanges(workflow, workflows, ruleIndex + 1, failingCopy));
+                }
+            }
+
+            return ranges;
         }
 
         private static (IEnumerable<Workflow> Workflows, IEnumerable<Part> Parts) ParseLines(IList<string> lines)
